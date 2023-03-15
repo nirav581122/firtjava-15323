94ce0c956a0ca0e82790405c9852748929abff53
nirav5811 <niravnamlee@gmail.com>
2023-03-15 22:05:25 +0000

    Last Name of the person who have opened the file has been modified

--- src/PersonalDetails.java
+++ src/PersonalDetails.java
@@ -1,7 +1,7 @@
 public class PersonalDetails {
     public static void main(String[] args) {
         System.out.printf("My First Name");
-        System.out.printf("Last Name:");
+        System.out.printf("Last Name of the person who have opened the file:");
         System.out.printf("Date of Birth");
         System.out.printf("Address");
 
