648d18703d070cc2cf580f97d775fc49d83f1146
nirav5811 <niravnamlee@gmail.com>
2023-03-15 21:56:17 +0000

    first 4 files are ready

---
+++ src/PersonalDetails.java
@@ -0,0 +1,11 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        System.out.printf("My First Name");
+        System.out.printf("Last Name:");
+        System.out.printf("Date of Birth");
+        System.out.printf("Address");
+
+
+    }
+
+}
